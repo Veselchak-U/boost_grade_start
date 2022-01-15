afcfdb66eb5a0d253a8d0cf99f3ed45a07393bb8
Dmitriy Usatov <usatov.dv@gmail.com>
2022-01-15 20:10:28 +0300

    improved error handle

--- lib/features/async_call/view/async_call_screen.dart
+++ lib/features/async_call/view/async_call_screen.dart
@@ -2,12 +2,12 @@ import 'package:boost_grade_start/app/asset/asset.dart';
 import 'package:boost_grade_start/app/l10n/l10n.dart';
 import 'package:boost_grade_start/app/view/components/bgs/bgs_progress_indicator.dart';
 import 'package:boost_grade_start/app/view/components/bgs/bgs_scaffold.dart';
+import 'package:boost_grade_start/app/view/components/bgs/bgs_snake_bar.dart';
 import 'package:boost_grade_start/features/async_call/model/photo_model.dart';
 import 'package:boost_grade_start/features/async_call/provider/data_provider.dart';
 import 'package:boost_grade_start/features/async_call/provider/data_provider_impl.dart';
 import 'package:boost_grade_start/features/async_call/view/async_call_screen_route.dart';
 import 'package:boost_grade_start/features/async_call/view/components/photo_card.dart';
-import 'package:boost_grade_start/util/debug.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -39,13 +39,13 @@ class _AsyncCallScreenState extends State<AsyncCallScreen> {
     });
   }
 
-  Future<void> _requestPhotos() async {
+  Future<void> _requestPhotos(BuildContext context) async {
     _updateProgress(true);
     try {
       final result = await _dataProvider.getPhotos();
       _items = result ?? [];
     } catch (error) {
-      out(error);
+      BgsSnakeBar(context, error);
     } finally {
       _updateProgress(false);
     }
@@ -76,7 +76,7 @@ class _AsyncCallScreenState extends State<AsyncCallScreen> {
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _requestPhotos,
+        onPressed: () => _requestPhotos(context),
         child: SvgPicture.asset(Asset.svg.iconEdit),
       ),
     );
